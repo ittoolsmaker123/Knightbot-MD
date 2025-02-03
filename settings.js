6a95a2c5720af3a7fc79deaf8f88ace83c21218c
ittoolsmaker123 <sarojji141@gmail.com>
2025-02-03 22:54:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "◄⏤͟͞ꭙͯ͢³̶👿⃪꯭̽𝗗꯭̽ͥ𝗘̽ͫ𝗠꯭𝗢⃪꯭̽𝗡⃯͓ ⸼⃪ ꯭˲̶☠️꯭᪳𓆪͓⃯",
+  botOwner: 'XD PANDA', // Your name
+  ownerNumber: '917091650914', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
